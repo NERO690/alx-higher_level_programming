2203534bb96157b261f65cc0b09695588309ca9e
NERO960 <nelsonchristmas35@gmail.com>
2023-10-11 14:05:28 -0700

    my class

---
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+
+def square_matrix_simple(matrix=[]):
+    """
+    A function named "computeSquare" that calculates the square of each integer in a matrix.
+    new_matrix = []
+    for col in matrix:
+        result = list(map(lambda x: x**2, col))
+        new_matrix.append(result)
+    return new_matrix
