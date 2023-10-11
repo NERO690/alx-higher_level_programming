84899a138c66fa2a7d66829d9e063e540590bed2
NERO960 <nelsonchristmas35@gmail.com>
2023-10-11 14:10:59 -0700

    My task

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -3,7 +3,9 @@
 
 def square_matrix_simple(matrix=[]):
     """
-    A function named "computeSquare" that calculates the square of each integer in a matrix.
+    wordA function that computes the square
+    value of all integers of a matrix.
+    """
     new_matrix = []
     for col in matrix:
         result = list(map(lambda x: x**2, col))
